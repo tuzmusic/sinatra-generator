3c0f019ac5d2dbb2615dc24710ce286b2590ab0f
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-27 19:43:44 -0500

    refactor to ClassGenerator; implement generate_migration

---
+++ _generated/db/migrate/20181127194222_create_songs.rb
@@ -0,0 +1,7 @@
+class Createsong < ActiveRecord::Migration
+  def change
+    create_table :song do |t|
+      t.string :name
+    end
+  end
+end
--- app/models/site_generator.rb
+++ app/models/site_generator.rb
@@ -11,31 +11,9 @@ class SiteGenerator
     FileUtils.mkdir_p path
   end
 
-  def generate(class_name)
-    generate_model_code(class_name)
-    generate_migration(class_name)
-  end
-
-  def generate_model_code(class_name)
-    code_template_str = File.read("app/templates/model.erb")
-    template = ERB.new(code_template_str)
-    code = template.result(binding)
-    new_path = FileUtils.mkdir_p "#{path}/app/models/"
-    filename = "#{new_path.last}/#{class_name.downcase}.rb"
-    File.write(filename, code)
-  end
-  
-  def generate_migration(class_name)
-    date_str = DateTime.now.strftime '%Y%m%d%H%M%S'
-    filename = "#{date_str}_create_#{class_name.downcase}s.rb"
-    migr_path = FileUtils.mkdir_p "#{path}/db/migrate"
-    full_path = "#{migr_path.last}/#{filename}"
-    
-    binding.pry
-  end
   
   def call
-    generate("Song")
+    ClassGenerator.new(path:@path, class_name:"Song").generate
   end
   
 end

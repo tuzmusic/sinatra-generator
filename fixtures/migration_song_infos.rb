0f830d1fc342765ce07a998b35d4d6c31fd6d7aa
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-29 12:49:22 -0500

    add migration fixture

---
+++ fixtures/migration_song_infos.rb
@@ -0,0 +1,9 @@
+# class CreateSongInfo < ActiveRecord::Base
+#   def change
+#     create_table :song_infos do |t|
+#       t.string :name
+#       t.string :artist_name
+#       t.integer :duration_in_seconds
+#     end
+#   end  
+# end

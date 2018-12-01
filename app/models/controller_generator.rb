3129ffec3ffa0884e507b20fc433e946b803ee93
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-30 20:57:11 -0500

    implement all_properties, but test is funny and fails

--- app/models/controller_generator.rb
+++ app/models/controller_generator.rb
@@ -6,6 +6,13 @@ class ControllerGenerator
     "@#{@class.table_name} = #{@class.name}.all"
   end
 
+  def all_properties
+    props = @class.belongs_to + @class.has_many_through.map{|p| p[:many]}
+    props.collect do |prop_name|
+      "@#{prop_name.underscore.pluralize} = #{prop_name}.all" 
+    end
+  end  
+
   def index_action
     %(get '/#{@class.table_name}' do
       @#{@class.table_name} = #{@class.name}.all

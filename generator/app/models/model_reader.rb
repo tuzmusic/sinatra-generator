38cf7348e03c006af7af6afe75430f01e1e5964e
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 09:48:57 -0500

    implement class_name_from_table_name

--- generator/app/models/model_reader.rb
+++ generator/app/models/model_reader.rb
@@ -5,13 +5,13 @@ class ModelReader
     file.scan(/class (\S+)/).first.first
   end  
 
-  def belongs_to
-    array = file.scan(/belongs_to :(\S+)/).first
-    class_names_from_array_of_table_names(array)
+  def class_name_from_table_name(str)
+    str.titleize.gsub(' ', '').singularize
   end
 
-  def class_names_from_array_of_table_names(arr)
-    arr.map {|p| p.titleize.gsub(' ', '').singularize}
+  def belongs_to
+    array = file.scan(/belongs_to :(\S+)/).first
+    .map {|p| class_name_from_table_name(p)}
   end
 
   def all_has_many
@@ -20,16 +20,16 @@ class ModelReader
   
   def has_many
     # Does not work if class name (singular) ends in S
-    array = all_has_many.select {|p| !p.include?('through')}
-    class_names_from_array_of_table_names(array)
+    all_has_many.select {|p| !p.include?('through')}
+    .map {|p| class_name_from_table_name(p)}
   end
   
   def all_through
     strs = all_has_many.select {|p| p.include?('through')}
     hashes = strs.map do |str|
       matches = str.scan(/(\w+), through: :(\w+)/).first
-      binding.pry
-      {many: matches[0].strip, through: matches[1].strip}
+      .map {|p| class_name_from_table_name(p)}
+      {many: matches[0].strip, through: matches[1].strip.pluralize}
     end    
   end
 

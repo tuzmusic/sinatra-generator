7fa414e6d273a46416e0a79421b737d9c9b47f50
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-03 21:00:35 -0500

    fix file references

---
+++ generator/app/models/model_reader.rb
@@ -0,0 +1,14 @@
+class ModelReader
+  
+  def generate_model(file)
+    
+  end
+
+  def all_files
+    Dir["app/models/*.rb"]
+  end
+
+  def run
+    
+  end
+end
--- spec/generator/contoller_generator_spec.rb
+++ spec/generator/contoller_generator_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../spec_helper'
-Dir[File.join(File.dirname(__FILE__), "../../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../../generator/app/models", "*.rb")].each {|f| require f}
 
 describe "ControllerGenerator" do
   

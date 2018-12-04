9684219d897919b90596bae87aaad5702a1f13be
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-03 20:46:15 -0500

    write Song class relationhips

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
-  
+  belongs_to :artist
+  has_many :genres, through: :song_genres
+  has_many :players, through: :song_players
+  has_many :verses
+  has_many :chorus
 end

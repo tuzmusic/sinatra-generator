53cd3ead1f169a4704cba3129148e22400cc3215
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 11:58:23 -0500

    create new site_generator file

---
+++ generator/app/models/site_generator.rb
@@ -0,0 +1,21 @@
+require 'date'
+require 'erb'
+require 'FileUtils'
+
+class SiteGenerator
+
+  attr_reader :classes 
+ 
+  def generate_generator_classes
+    paths = Dir["/app/models/*.rb"]
+    @classes = paths.map do |path|
+      ModelReader.new(path).create_generator_class
+    end
+  end
+
+  def call
+    generate_generator_classes
+    generate_migrations
+    generate_controllers
+  end
+end

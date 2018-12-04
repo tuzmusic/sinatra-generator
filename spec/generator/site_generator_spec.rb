4ef8b3f33087ea3e60fc66ad17af8babf0392b5e
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 12:17:58 -0500

    spec generate_migrations

--- spec/generator/site_generator_spec.rb
+++ spec/generator/site_generator_spec.rb
@@ -2,11 +2,21 @@ require_relative '../spec_helper'
 Dir[File.join(File.dirname(__FILE__), "../../generator/app/models", "*.rb")].each {|f| require f}
 
 describe "SiteGenerator" do
+  
   let(:sg) { SiteGenerator.new }
+
   describe "generate_generator_classes" do
-   it "creates a GeneratorClass object for each class in the models folder and stores them in the @classes array" do
-    sg.generate_generator_classes
+    it "creates a GeneratorClass object for each class in the models folder and stores them in the @classes array" do
+      sg.generate_generator_classes
       expect(sg.classes.count).to eq(Dir["app/models/*.rb"].count)
-   end
+    end
+
+    describe "generate_migrations" do
+      it "creates migration files for each class" do
+        sg.generate_generator_classes
+        sg.generate_migrations
+        expect(Dir["db/migrate/*.rb"].count).to eq(Dir["app/models/*.rb"].count)
+      end    
+    end
   end
 end

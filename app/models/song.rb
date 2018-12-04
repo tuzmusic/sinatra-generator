58032e13b64d33c721bb5da1dbdef9d771fece1e
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-03 20:48:43 -0500

    write genre class

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :genres, through: :song_genres
+  has_many :song_genres
   has_many :players, through: :song_players
+  has_many :song_players
   has_many :verses
   has_many :chorus
 end

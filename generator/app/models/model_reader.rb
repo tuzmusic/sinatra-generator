3eea5f0846a776039690e9f896a45ae82402d37d
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 09:41:00 -0500

    implement class_names_from_array_of_table_names

--- generator/app/models/model_reader.rb
+++ generator/app/models/model_reader.rb
@@ -9,27 +9,32 @@ class ModelReader
     file.scan(/belongs_to :(\S+)/).first.map {|t| t.titleize}
   end
 
+  def class_names_from_array_of_table_names(arr)
+    # binding.pry
+    arr.map {|p| p.titleize.gsub(' ', '').singularize}
+  end
+
   def all_has_many
     file.scan(/has_many :(.+)/).map {|p| p.first.strip}
   end
   
   def has_many
     # Does not work if class name (singular) ends in S
-    all_has_many.select {|p| !p.include?('through')}
-    .map { |p| p.titleize.gsub(' ', '').singularize }
+    array = all_has_many.select {|p| !p.include?('through')}
+    class_names_from_array_of_table_names(array)
+    # .map { |p| p.titleize.gsub(' ', '').singularize }
   end
   
   def all_through
-    
+    strs = all_has_many.select {|p| p.include?('through')}
+    hashes = strs.map do |str|
+      matches = str.scan(/(\w+), through: :(\w+)/).first
+      binding.pry
+      {many: matches[0].strip, through: matches[1].strip}
+    end    
   end
 
   def has_many_through
-    strs = all_has_many.select {|p| p.include?('through')}
-    binding.pry
-    hashes = strs.map do |str|
-      matches = str.scan(/has_many :(\w+), through: :(\w+)/)
-      {many: matches[0], through: matches[1]}
-    end
   end
   
   def many_through_join

58e70ccde552f4dce2074fe4be2b5146169dee99
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-03 21:28:43 -0500

    implement class_name

--- generator/app/models/model_reader.rb
+++ generator/app/models/model_reader.rb
@@ -1,6 +1,32 @@
 class ModelReader
   attr_accessor :filepath, :class, :file
 
+  def class_name
+    binding.pry
+    file.scan(/class (\S+)/).first.first
+  end  
+
+  def belongs_to
+    
+  end
+  
+  def has_many
+    
+  end
+  
+  def has_many_through
+    
+  end
+  
+  def many_through_join
+    
+  end
+
+  def properties
+    
+  end
+
+
   def initialize(filepath)
     @filepath = filepath  
     @file = File.read(filepath)
